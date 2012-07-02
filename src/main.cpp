7d6d5763e3b054eb3f3d9bc94f4c85e642310d06
rwhwilliam <rwhyr4@mst.edu>
2012-07-01 19:04:51 -0500

    Updated the CMake file to copy over the resources (music, config, maps, etc) over to the proper destination folders for windows and unix builds

---
+++ src/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include "SDL.h"
+
+using namespace std;
+
+SDL_Event event;
+
+int main(int argc, char* args[])
+{
+	if(SDL_Init(SDL_INIT_EVERYTHING) == -1)
+		return 0;
+
+	if(SDL_EnableUNICODE( SDL_ENABLE ) == -1)
+		return 0;
+
+	SDL_Surface* screen = SDL_SetVideoMode(640, 480, 32, SDL_SWSURFACE);
+
+	bool quit = false;
+
+	while(!quit)
+	{
+		while(SDL_PollEvent(&event))
+		{
+			if(event.type == SDL_QUIT)
+			{
+				quit = true;
+			}
+		}
+
+		Uint8 *keystates = SDL_GetKeyState(NULL);
+
+		SDL_FillRect(screen, &screen->clip_rect, SDL_MapRGB(screen->format, 0x00, 0x00, 0x00));
+
+		SDL_Flip(screen);
+	}
+
+	SDL_EnableUNICODE(SDL_DISABLE);
+
+	SDL_Quit();
+
+	return 0;
+}
