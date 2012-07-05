c5f20ec1d656febd1d107b7da86b1affd9123f8a
rwhwilliam <rwhyr4@mst.edu>
2012-07-04 22:34:42 -0500

    Got the beginnings of some stuff, also added a placeholder caste to start off designing the actual game.

---
+++ src/Villages/States/SimState.h
@@ -0,0 +1,49 @@
+/**************************************************************************************************
+* Copyright 2012 Ryan Holzum                                                                      *
+*                                                                                                 *
+* This file is part of Villages.                                                                  *
+*                                                                                                 *
+* Villages is free software: you can redistribute it and/or modify it under the terms of the      *
+* GNU General Public License as published by the Free Software Foundation, either version 3 of    *
+* the License, or (at your option) any later version.                                             *
+*                                                                                                 *
+* Vilages is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without    *
+* even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the       *
+* GNU General Public License for more details.                                                    *
+*                                                                                                 *
+* You should have received a copy of the GNU General Public License along with Vilages.           *
+* If not, see http://www.gnu.org/licenses/.                                                       *
+**************************************************************************************************/
+
+#ifndef SIMSTATE_H
+#define SIMSTATE_H
+
+#include <string>
+
+#include "SDL.h"
+
+#include "Engine/State/State.h"
+#include "Engine/Graphics/TileMap.h"
+
+using namespace std;
+
+class SimState : public State
+{
+public:
+	SimState(string path, int width, int height, int xloc, int yloc);
+	~SimState();
+
+	SimState(const SimState& data);
+	SimState& operator=(const SimState* rhs);
+
+	void update(float time, Uint8* keystrokes);
+	void raiseEvent(SDL_Event event);
+	void draw();
+
+	TileMap* getMap();
+
+private:
+
+};
+
+#endif
