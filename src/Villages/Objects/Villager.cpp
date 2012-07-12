29918279231e40d7d970d46b7238186f7ec05e78
rwhwilliam <rwhyr4@mst.edu>
2012-07-11 21:21:23 -0500

    Some updated data files and CMake list, but more importantly some psd files for farm images.  Also the start of a villager class that will define a person in the city.

---
+++ src/Villages/Objects/Villager.cpp
@@ -0,0 +1,42 @@
+/**************************************************************************************************
+* Copyright 2012 Ryan Holzum                                                                      *
+*                                                                                                 *
+* This file is part of Villages.                                                                  *
+*                                                                                                 *
+* Villages is free software: you can redistribute it and/or modify it under the terms of the      *
+* GNU General Public License as published by the Free Software Foundation, either version 3 of    *
+* the License, or (at your option) any later version.                                             *
+*                                                                                                 *
+* Vilages is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without    *
+* even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the       *
+* GNU General Public License for more details.                                                    *
+*                                                                                                 *
+* You should have received a copy of the GNU General Public License along with Vilages.           *
+* If not, see http://www.gnu.org/licenses/.                                                       *
+**************************************************************************************************/
+
+#include "Villager.h"
+
+#include "Engine/Util/VillageException.h"
+
+Villager::Villager(Job job, House* residence)
+{
+	Villager::job = job;
+
+	Villager::residence = residence;
+}
+
+Villager::~Villager()
+{
+	Villager::residence = NULL;
+}
+
+Villager::Villager(const Villager& data)
+{
+	throw VillageException("Villager Copy Constructor");
+}
+
+Villager& Villager::operator=(const Villager* rhs)
+{
+	throw VillageException("Villager Assignment Operator");
+}
